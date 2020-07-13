3aae7442a96f5cc888674209446d08e0661804bf
Jesse Pires <jessepires2010@gmail.com>
2020-07-12 22:05:30 -0300

    Login and Logout implementation

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,26 +1,102 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import { Button, Grid } from '@material-ui/core';
-import { ReactComponent as Logo } from './logo-react-zzaria.svg';
-import { Container } from './style';
+import { Container, StyledLogo, GithubButton } from './style';
 
-const Login = () => (
-  <>
-    <Container>
-      <Grid
-        container
-        direction='column'
-        alignItems='center'
-        spacing={ 5 }
-      >
-        <Grid item >
-          <Logo />
-        </Grid>
-        <Grid item >
-          <Button>Entrar com o Github</Button>
-        </Grid>
-      </Grid>
-    </Container>
-  </>
-);
+var firebaseConfig = {
+  apiKey: 'AIzaSyA9HGweb4P6k0CoIL4sOZqKVjnyCbxNinE',
+  authDomain: 'reactzzaria-260e2.firebaseapp.com',
+  databaseURL: 'https://reactzzaria-260e2.firebaseio.com',
+  projectId: 'reactzzaria-260e2',
+  storageBucket: 'reactzzaria-260e2.appspot.com',
+  messagingSenderId: '981257987857',
+  appId: '1:981257987857:web:023004f63231c954a50eb0',
+  measurementId: 'G-M9ZSPQ4YWN'
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+class Login extends PureComponent {
+  state = {
+    isUserLoggedIn: false,
+    user: null
+  };
+
+  componentDidMount () {
+    firebase.auth().onAuthStateChanged((user) => {
+      console.log('dados do usuário:', user);
+      this.setState({
+        isUserLoggedIn: !!user,
+        user
+      });
+    });
+  }
+
+  logout = () => {
+    firebase.auth().signOut().then(() => {
+      console.log('deslogou');
+      this.setState({
+        isUserLoggedIn: false,
+        user: null
+      });
+    });
+  }
+
+  login () {
+    const provider = new firebase.auth.GithubAuthProvider();
+    firebase.auth().signInWithRedirect(provider);
+  }
+
+  render () {
+    const { isUserLoggedIn, user } = this.state;
+
+    return (
+      <>
+        <Container>
+          <Grid
+            container
+            justify='center'
+            spacing={ 5 }
+          >
+            <Grid item >
+              <StyledLogo />
+            </Grid>
+
+            <Grid
+              item
+              xs={ 12 }
+              container
+              justify='center'
+            >
+
+              {isUserLoggedIn && (
+                <>
+                  <pre>{ user.displayName }</pre>
+
+                  <Button
+                    variant='contained'
+                    onClick={ this.logout }
+                  >
+                    Sair
+                  </Button>
+                </>
+              )}
+
+              { !isUserLoggedIn && (
+                <GithubButton
+                  onClick={ this.login }
+                >
+                  Entrar com o Github
+                </GithubButton>
+              )}
+            </Grid>
+          </Grid>
+        </Container>
+      </>
+    );
+  }
+}
 
 export default Login
--- src/pages/login/style.js
+++ src/pages/login/style.js
@@ -1,5 +1,23 @@
 import styled from 'styled-components';
+import { ReactComponent as Logo } from './logo-react-zzaria.svg';
+import { Button } from '@material-ui/core';
 
 export const Container = styled.div`
   padding: 20px;
+`;
+
+export const StyledLogo = styled(Logo)`
+  width: 100%;
+`;
+
+export const GithubButton = styled(Button).attrs({
+  variant: 'contained',
+  fullWidth: true
+})`
+  && {
+    font-size: 25px;
+    padding: 15px;
+    max-width: 430px;
+    text-transform: none;
+  }
 `;
