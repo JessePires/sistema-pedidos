f7d84d53d0f883233a7900dc5b7db75e7e31c959
Jesse Pires <jessepires2010@gmail.com>
2020-07-14 20:52:18 -0300

    Main Page content markup and style

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,18 +1,30 @@
 import React, { useState, useContext } from 'react';
-import { LogoContainer, Logo, Toolbar } from './styles.js';
 import {
   AppBar,
   IconButton,
   Menu,
   MenuItem,
-  Typography
+  Typography,
+  Grid,
+  withStyles
 } from '@material-ui/core';
 import { AccountCircle } from '@material-ui/icons';
 import { AuthContext } from '../../contexts/auth';
+import {
+  LogoContainer,
+  Logo,
+  Toolbar,
+  Content,
+  PaperPizza,
+  Pizza,
+  PizzaText,
+  Divider
+} from './styles.js';
 
 function Main () {
   const [ anchorElement, setAnchorElement ] = useState(null);
   const { userInfo, logout } = useContext(AuthContext);
+  const userName = userInfo.user.displayName.split(' ')[0];
 
   const handleOpenMenu = (e) => {
     setAnchorElement(e.target);
@@ -23,33 +35,110 @@ function Main () {
   };
 
   return (
-    <AppBar>
-      <Toolbar>
-        <LogoContainer>
-          <Logo />
-        </LogoContainer>
+    <>
+      <AppBar>
+        <Toolbar>
+          <LogoContainer>
+            <Logo />
+          </LogoContainer>
 
-        <Typography color='inherit' >
-          Olá { userInfo.user.displayName.split(' ')[0] } :)
-        </Typography>
+          <Typography color='inherit' >
+            Olá { userName } :)
+          </Typography>
 
-        <IconButton
-          color='inherit'
-          onClick={ handleOpenMenu }
+          <IconButton
+            color='inherit'
+            onClick={ handleOpenMenu }
+          >
+            <AccountCircle />
+          </IconButton>
+
+          <Menu
+            open={ Boolean(anchorElement) }
+            onClose={ handleClose }
+            anchorEl={ anchorElement }
+          >
+            <MenuItem onClick={ logout } >Sair</MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+
+      <Spacer />
+
+      <Content>
+        <Grid
+          container
+          direction='column'
+          alignItems='center'
         >
-          <AccountCircle />
-        </IconButton>
+          <Typography variant='h4' gutterBottom >
+            O que vai ser hoje, {userName}?
+          </Typography>
 
-        <Menu
-          open={ Boolean(anchorElement) }
-          onClose={ handleClose }
-          anchorEl={ anchorElement }
-        >
-          <MenuItem onClick={ logout } >Sair</MenuItem>
-        </Menu>
-      </Toolbar>
-    </AppBar>
+          <Typography variant='h5' gutterBottom >
+            Escolha o tamanho da pizza:
+          </Typography>
+        </Grid>
+
+        <Grid container spacing={ 2 } >
+          {pizzasDesc.map((pizza) => (
+
+            <Grid item key={ pizza.id } xs={ 4 } >
+              <PaperPizza >
+                <Pizza>
+                  <PizzaText>
+                    { pizza.size }cm
+                  </PizzaText>
+                </Pizza>
+
+                <Divider />
+
+                <Typography variant='h6' >{ pizza.name }</Typography>
+                <Typography>
+                  { pizza.slices } fatias, { pizza.flavours } sabores
+                </Typography>
+
+              </PaperPizza>
+            </Grid>
+          ))}
+        </Grid>
+      </Content>
+    </>
   );
 }
+
+const pizzasDesc = [
+  {
+    id: 0,
+    name: 'Pequena',
+    size: 28,
+    slices: 2,
+    flavours: 1
+  },
+
+  {
+    id: 1,
+    name: 'Média',
+    size: 30,
+    slices: 6,
+    flavours: 2
+  },
+
+  {
+    id: 2,
+    name: 'Grande',
+    size: 32,
+    slices: 8,
+    flavours: 3
+  }
+];
+
+const style = (theme) => ({
+  main: theme.mixins.toolbar
+});
+
+const Spacer = withStyles(style)(({ classes }) => (
+  <div className={ classes.main } />
+));
 
 export default Main;
--- src/pages/main/styles.js
+++ src/pages/main/styles.js
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
-import { Toolbar as MaterialToolbar } from '@material-ui/core';
+import {
+  Toolbar as MaterialToolbar,
+  Divider as MaterialDivider,
+  Paper,
+  Typography
+} from '@material-ui/core';
 import { ReactComponent as MainLogo } from '../../images/logo-react-zzaria.svg';
 
 export const LogoContainer = styled.div`
@@ -23,4 +28,63 @@ export const Toolbar = styled(MaterialToolbar)`
   margin: 0 auto;
   max-width: 960px;
   width: 100%;
+`;
+
+export const Divider = styled(MaterialDivider)`
+  margin: 20px 0;
+  width: 100%;
+`;
+
+export const PaperPizza = styled(Paper)`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  padding: 20px 0;
+`;
+
+export const Pizza = styled.div`
+  height: 200px;
+  width: 200px;
+  border: 1px solid #ccc;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+
+  &::before,
+  &::after {
+    content: '';
+    background: #ccc;
+    position: absolute;
+    transform: rotate(45deg);
+  }
+
+  &::before {
+    width: 160px;
+    height: 1px;
+  }
+
+  &::after {
+    height: 160px;
+    width: 1px;
+  }
+`;
+
+export const PizzaText = styled(Typography).attrs({
+  variant: 'h5'
+})`
+  height: 75px;
+  width: 75px;
+  border-radius: 50%;
+  background: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  z-index: 1;
+`;
+
+export const Content = styled.main`
+  padding: 20px;
 `;
