eb4c3bfb6196f39ae2111abe692f99e6b5d7fb6f
Jesse Pires <jessepires2010@gmail.com>
2020-07-22 17:01:21 -0300

    Get data based on CEP

--- src/pages/checkout/formAddress.js
+++ src/pages/checkout/formAddress.js
@@ -1,9 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Grid } from '@material-ui/core';
 import TextField from './textField';
 
 function FormAddress () {
   const [ cep, setCep ] = useState('');
+
+  useEffect(() => {
+    async function fetchAddress () {
+      if (cep.length < 9) {
+        return;
+      }
+
+      console.log('buscar cep:', cep);
+
+      const data = await fetch(
+        `https://ws.apicep.com/cep/${ cep }.json`
+      );
+
+      const result = await data.json();
+      console.log(result);
+    };
+
+    fetchAddress();
+  }, [cep]);
 
   function handleChangeCep (e) {
     setCep(cepMask(e.target.value));
